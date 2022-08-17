04cc7dc9f2e7c93766a1103dedcf1fb90f12dfaa
Josedevelope <jostylemrs@gmail.com>
2022-08-17 14:32:57 +0100

    update everything

---
+++ resources/homecontroller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\{
+    Curso
+};
+
+use Illuminate\Http\Request;
+
+class homecontroller extends Controller
+{
+    //
+
+    public function index()
+    {
+        //$curso=Curso::get();
+//return view('home', compact('curso'));
+return view("welcome");
+    }
+}
